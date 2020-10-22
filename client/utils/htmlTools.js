972070f898e5b48013179eb3e52852b035296df3
286208466 <286208466@qq.com>
2020-10-22 18:39:06 +0800

    1

--- client/utils/htmlTools.js
+++ client/utils/htmlTools.js
@@ -1,4 +1,12 @@
-/* eslint-disable prettier/prettier */
+/**
+ * @param {string} val
+ * @returns {string}
+ */
+export function html2Text(val) {
+  const div = document.createElement("div");
+  div.innerHTML = val;
+  return div.textContent || div.innerText;
+}
 //返回顶部
 export function goTop() {
   /*(function smoothscroll(){  
@@ -13,30 +21,32 @@ export function goTop() {
     .stop()
     .animate(
       {
-        scrollTop: 0
+        scrollTop: 0,
       },
       300
     );
 }
 
-/** 
- * 得到一个元素的left坐标值. 
- * @param {dom对象} obj 
- * @return {位置值} 
- */  
-function getLeft(obj){   
-    var offset=e.offsetLeft;   
-    if(e.offsetParent!=null) offset+=getLeft(e.offsetParent);   
-    return offset;   
-}   
-  
-/** 
-* 得到一个元素的绝对位置的top坐标值. 
-* @param {dom对象} obj 
-* @return {位置值} 
-*/  
-function getTop(obj){   
-    var offset=e.offsetTop;   
-    if(e.offsetParent!=null) offset+=getTop(e.offsetParent);   
-    return offset;   
-} 
+/**
+ * 得到一个元素的left坐标值.
+ * @param {dom对象} obj
+ * @return {位置值}
+ */
+
+export function getLeft(e) {
+  var offset = e.offsetLeft;
+  if (e.offsetParent != null) offset += getLeft(e.offsetParent);
+  return offset;
+}
+
+/**
+ * 得到一个元素的绝对位置的top坐标值.
+ * @param {dom对象} obj
+ * @return {位置值}
+ */
+
+export function getTop(e) {
+  var offset = e.offsetTop;
+  if (e.offsetParent != null) offset += getTop(e.offsetParent);
+  return offset;
+}
